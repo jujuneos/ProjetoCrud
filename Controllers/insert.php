306bb45ba49a6ac080b27f1bb203e838a0e46d84
jujuneos <lcjuniorconceicao@gmail.com>
2020-10-11 23:32:34 -0300

    Melhorando interface, adicionando página de sucesso

--- Controllers/insert.php
+++ Controllers/insert.php
@@ -1,13 +1,12 @@
 <?php
 
-	include('dataconfig.php');
+	include('../Models/dataconfig.php');
+	include('../Views/insert.html');
 	$nome = $_POST["nome"];
 	$email = $_POST["email"];
 	$telefone = $_POST["telefone"];
 
 	$statement = $pdo->prepare("INSERT INTO cliente (nome, email, telefone) VALUES (?,?,?)");
 	$statement->execute([$nome, $email, $telefone]);
-
-	echo "Cliente cadastrado com sucesso";
 	
 ?>
